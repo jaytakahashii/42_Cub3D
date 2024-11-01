92f37f4ee819e1ff10ffd20e0b85531941ce94fb
Jay Takahashi (42ubuntu) <qsakura14555@gmail.com>
2024-11-01 18:22:38 +0900

    feat: マップに合わせて壁を表示

--- src/raycasting.c
+++ src/raycasting.c
@@ -2,32 +2,215 @@
 
 // raycasting.c
 
+/*
+** 線分の向きを4つに分類する
+** 上45度, 右45度, 下45度, 左45度
+** line: 線分
+** return: 0: 上, 1: 右, 2: 下, 3: 左
+*/
+int	segment_direction(t_line_segment line)
+{
+	double	angle;
+
+	angle = atan2(line.end.y - line.start.y, line.end.x - line.start.x);
+	if (angle >= -M_PI / 4 && angle < M_PI / 4)
+		return (1);
+	else if (angle >= M_PI / 4 && angle < 3 * M_PI / 4)
+		return (0);
+	else if (angle >= 3 * M_PI / 4 || angle < -3 * M_PI / 4)
+		return (3);
+	else
+		return (2);
+}
+
+/*
+** DDAアルゴリズム 上向き
+** レイと壁の交点を求める
+** game: ゲーム構造体
+** ray: レイ
+** num: レイの番号
+** angle: レイの角度
+*/
+void	dda_0(t_game *game, t_line_segment ray, int num, double angle)
+{
+	int			map_x;
+	int			map_y;
+	t_vector	wall;
+
+	map_y = (int)(ray.start.y / TILE_SIZE);
+	while (1)
+	{
+		map_x = (int)(line_calc_x(ray.line, map_y * TILE_SIZE) / TILE_SIZE);
+		if (game->map_info->map[map_y][map_x] == '1')
+		{
+			map_x++;
+			wall.y = line_calc_y(ray.line, map_x * TILE_SIZE);
+			wall.x = map_x * TILE_SIZE;
+			if (ray.start.x < wall.x) // 西の壁
+				draw_wall(game, num, angle, vector_len(vector_from_to(ray.start, wall)), MGREEN);
+			else // 東の壁
+				draw_wall(game, num, angle, vector_len(vector_from_to(ray.start, wall)), MBLUE);
+			return;
+		}
+		if (game->map_info->map[map_y - 1][map_x] == '1') // 南の壁
+		{
+			wall.y = map_y * TILE_SIZE;
+			wall.x = line_calc_x(ray.line, map_y * TILE_SIZE);
+			draw_wall(game, num, angle, vector_len(vector_from_to(ray.start, wall)), MWHITE);
+			return;
+		}
+		map_y--;
+	}
+}
+
+/*
+** DDAアルゴリズム 右向き
+** レイと壁の交点を求める
+** game: ゲーム構造体
+** ray: レイ
+** num: レイの番号
+** angle: レイの角度
+*/
+void	dda_1(t_game *game, t_line_segment ray, int num, double angle)
+{
+	int			map_x;
+	int			map_y;
+	t_vector	wall;
+
+	map_x = (int)(ray.start.x / TILE_SIZE);
+	while (1)
+	{
+		map_y = (int)(line_calc_y(ray.line, map_x * TILE_SIZE) / TILE_SIZE);
+		if (game->map_info->map[map_y][map_x] == '1')
+		{
+			map_y++;
+			wall.y = map_y * TILE_SIZE;
+			wall.x = line_calc_x(ray.line, map_y * TILE_SIZE);
+			if (ray.start.y < wall.y) // 北の壁
+				draw_wall(game, num, angle, vector_len(vector_from_to(ray.start, wall)), MYELLOW);
+			else // 南の壁
+				draw_wall(game, num, angle, vector_len(vector_from_to(ray.start, wall)), MWHITE);
+			return;
+		}
+		if (game->map_info->map[map_y][map_x + 1] == '1') // 西の壁
+		{
+			wall.y = line_calc_y(ray.line, (map_x + 1) * TILE_SIZE);
+			wall.x = (map_x + 1) * TILE_SIZE;
+			draw_wall(game, num, angle, vector_len(vector_from_to(ray.start, wall)), MGREEN);
+			return;
+		}
+		map_x++;
+	}
+}
+
+/*
+** DDAアルゴリズム 下向き
+** レイと壁の交点を求める
+** game: ゲーム構造体
+** ray: レイ
+** num: レイの番号
+** angle: レイの角度
+*/
+void	dda_2(t_game *game, t_line_segment ray, int num, double angle)
+{
+	int			map_x;
+	int			map_y;
+	t_vector	wall;
+
+	map_y = (int)(ray.start.y / TILE_SIZE);
+	while (1)
+	{
+		map_x = (int)(line_calc_x(ray.line, map_y * TILE_SIZE) / TILE_SIZE);
+		if (game->map_info->map[map_y][map_x] == '1')
+		{
+			map_x++;
+			wall.y = line_calc_y(ray.line, map_x * TILE_SIZE);
+			wall.x = map_x * TILE_SIZE;
+			if (ray.start.x < wall.x) // 西の壁
+				draw_wall(game, num, angle, vector_len(vector_from_to(ray.start, wall)), MGREEN);
+			else // 東の壁
+				draw_wall(game, num, angle, vector_len(vector_from_to(ray.start, wall)), MBLUE);
+			return;
+		}
+		if (game->map_info->map[map_y + 1][map_x] == '1') // 北の壁
+		{
+			wall.y = map_y * TILE_SIZE;
+			wall.x = line_calc_x(ray.line, map_y * TILE_SIZE);
+			draw_wall(game, num, angle, vector_len(vector_from_to(ray.start, wall)), MYELLOW);
+			return;
+		}
+		map_y++;
+	}
+}
+
+/*
+** DDAアルゴリズム 左向き
+** レイと壁の交点を求める
+** game: ゲーム構造体
+** ray: レイ
+** num: レイの番号
+** angle: レイの角度
+*/
+void	dda_3(t_game *game, t_line_segment ray, int num, double angle)
+{
+	int			map_x;
+	int			map_y;
+	t_vector	wall;
+
+	map_x = (int)(ray.start.x / TILE_SIZE);
+	while (1)
+	{
+		map_y = (int)(line_calc_y(ray.line, map_x * TILE_SIZE) / TILE_SIZE);
+		if (game->map_info->map[map_y][map_x] == '1')
+		{
+			map_y++;
+			wall.y = map_y * TILE_SIZE;
+			wall.x = line_calc_x(ray.line, map_y * TILE_SIZE);
+			if (ray.start.y < wall.y) // 北の壁
+				draw_wall(game, num, angle, vector_len(vector_from_to(ray.start, wall)), MYELLOW);
+			else // 南の壁
+				draw_wall(game, num, angle, vector_len(vector_from_to(ray.start, wall)), MWHITE);
+			return;
+		}
+		if (game->map_info->map[map_y][map_x - 1] == '1') // 東の壁
+		{
+			wall.y = line_calc_y(ray.line, (map_x - 1) * TILE_SIZE);
+			wall.x = (map_x - 1) * TILE_SIZE;
+			draw_wall(game, num, angle, vector_len(vector_from_to(ray.start, wall)), MBLUE);
+			return;
+		}
+		map_x--;
+	}
+}
+
+/*
+** 壁との当たり判定を行う関数
+** game: ゲーム構造体
+** ray: レイ
+** num: レイの番号
+** angle: レイの角度
+*/
 void	check_wall(t_game *game, t_line_segment ray, int num, double angle)
 {
-	// printf("ray.start.x: %f, ray.start.y: %f\n", ray.start.x, ray.start.y);
-	// printf("ray.end.x: %f, ray.end.y: %f\n", ray.end.x, ray.end.y);
-	// printf("ray.line.inclination: %f\n", ray.line.inclination);
-	if (-1 <= ray.line.inclination && ray.line.inclination <= 1
-		&& ray.start.x <= ray.end.x)
+	if (segment_direction(ray) == 0)
 	{
-		printf("%d\n", 1);
+		dda_0(game, ray, num, angle);
 	}
-	else if (-1 <= ray.line.inclination && ray.line.inclination <= 1
-		&& ray.start.x > ray.end.x)
+	else if (segment_direction(ray) == 1)
 	{
-		printf("%d\n", 3);
+		// 右向き
+		dda_1(game, ray, num, angle);
 	}
-	else if (ray.start.y <= ray.end.y)
+	else if (segment_direction(ray) == 2)
 	{
-		printf("%d\n", 4);
+		// 下向き
+		dda_2(game, ray, num, angle);
 	}
 	else
 	{
-		printf("%d\n", 2);
+		// 左向き
+		dda_3(game, ray, num, angle);
 	}
-	(void)game;
-	(void)num;
-	(void)angle;
 }
 
 /*
@@ -66,7 +249,7 @@ void	raycasting(t_game *game, t_player *player)
 ** angle: レイの角度
 ** distance: 壁までの距離
 */
-void	draw_wall(t_game *game, int num, double angle, double distance)
+void	draw_wall(t_game *game, int num, double angle, double distance, int color)
 {
 	t_vector	start;
 	t_vector	size;
@@ -76,5 +259,5 @@ void	draw_wall(t_game *game, int num, double angle, double distance)
 	start = vector_init(WIN_WIDTH / 2, WIN_HEIGHT / 2);
 	size = vector_init(1, rate);
 	start.x += num;
-	draw_rect(game, start, size, MBLUE);
+	draw_rect(game, start, size, color);
 }
