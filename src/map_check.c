828aeeb27f9d767006c718f62ebf4bbfb17c0ab9
kosnakam <kosnakam@c3r1s3.42tokyo.jp>
2024-11-08 13:22:25 +0900

    norm: map.cのファイル分割

---
+++ src/map_check.c
@@ -0,0 +1,77 @@
+#include "cub3D.h"
+
+void	set_player_info(t_game *game, char spell, t_vector p_pos)
+{
+	double	p_angle;
+
+	p_angle = 0;
+	if (spell == 'N')
+		p_angle = NORTH;
+	else if (spell == 'S')
+		p_angle = SOUTH;
+	else if (spell == 'W')
+		p_angle = WEST;
+	else if (spell == 'E')
+		p_angle = EAST;
+	game->player = player_init(p_pos.x * TILE_SIZE + TILE_SIZE / 2,
+			p_pos.y * TILE_SIZE + TILE_SIZE / 2, p_angle);
+}
+
+int	spell_check(char spell, int mode)
+{
+	if (mode == 1)
+	{
+		if (spell == '0' || spell == '1' || spell == '\n'
+			|| spell == ' ' || spell == 'N' || spell == 'S'
+			|| spell == 'W' || spell == 'E')
+		{
+			if (spell == 'N' || spell == 'S'
+				|| spell == 'W' || spell == 'E')
+				return (1);
+			return (2);
+		}
+	}
+	return (0);
+}
+
+int	map_spell_check(t_game *game, char **map)
+{
+	int	y;
+	int	x;
+
+	y = -1;
+	while (map[++y])
+	{
+		x = -1;
+		while (map[y] && map[y][++x])
+		{
+			if (spell_check(map[y][x], 1) == 1)
+			{
+				set_player_info(game, map[y][x], vector_init(x, y));
+			}
+			else if (spell_check(map[y][x], 1) == 0)
+				return (1);
+		}
+	}
+	return (0);
+}
+
+void	map_check(t_game *game, t_map *map_info)
+{
+	int				flag;
+	unsigned int	count;
+
+	flag = 0;
+	count = 0;
+	if (!map_info->map || !map_info->map[0])
+		error_exit_free("Map is not found", NULL, game->alloc);
+	if (!map_info->no || !map_info->so || !map_info->we || !map_info->ea
+		|| map_info->f == -1 || map_info->c == -1)
+		error_exit_free("Map information is not found", NULL, game->alloc);
+	if (map_spell_check(game, map_info->map))
+		error_exit_free("Invalid map", NULL, game->alloc);
+	wall_check(map_info->map_tmp, (int)game->player.pos.y / TILE_SIZE,
+		(int)game->player.pos.x / TILE_SIZE, &count, &flag);
+	if (flag)
+		error_exit_free("Invalid map", NULL, game->alloc);
+}
---
+++ src/wall.c
@@ -0,0 +1,38 @@
+#include "cub3D.h"
+
+int	wall_spell_check(char **map, int y, int x)
+{
+	if (!map[y + 1][x] || !map[y - 1][x] || !map[y][x + 1]
+		|| !map[y][x - 1])
+		return (1);
+	if (ft_isspace(map[y + 1][x]) || ft_isspace(map[y - 1][x])
+		|| ft_isspace(map[y][x + 1]) || ft_isspace(map[y][x - 1]))
+		return (1);
+	return (0);
+}
+
+void	wall_check(char **map, int y, int x, unsigned int *count, int *flag)
+{
+	*count += 1;
+	// todo: ほんとに一萬でいいか議論
+	if (*count > 10000)
+	{
+		*flag = 1;
+		return ;
+	}
+	if (!map || !map[y] || !map[y][x] || map[y][x] == '1')
+		return ;
+	else
+	{
+		map[y][x] = '1';
+		if (wall_spell_check(map, y, x))
+		{
+			*flag = 1;
+			return ;
+		}
+		wall_check(map, y + 1, x, count, flag);
+		wall_check(map, y - 1, x, count, flag);
+		wall_check(map, y, x + 1, count, flag);
+		wall_check(map, y, x - 1, count, flag);
+	}
+}
