29cff60afb1c4371ab522b4e45f835e9e0eca5a8
kokosnakam <unknown@c3r1s3.42tokyo.jp>
2024-12-06 14:47:22 +0900

    strdup

--- src/map.c
+++ src/map.c
@@ -12,31 +12,33 @@ int	ft_open(char *argv)
 
 void	map_info_init(t_map **map_info, t_allocations **alloc)
 {
-	int	j;
-
 	int (i) = -1;
 	*map_info = (t_map *)malloxit(sizeof(t_map), alloc);
 	(*map_info)->map = (char **)malloxit(sizeof(char *) * OPEN_MAX, alloc);
 	(*map_info)->map_tmp = (char **)malloxit(sizeof(char *) * OPEN_MAX, alloc);
 	while (++i < OPEN_MAX)
 	{
-		j = -1;
+		int (j) = -1;
 		(*map_info)->map[i] = (char *)malloxit(sizeof(char) * OPEN_MAX, alloc);
-		(*map_info)->map_tmp[i] = (char *)malloxit(sizeof(char)
-				* OPEN_MAX, alloc);
+		// (*map_info)->map_tmp[i] = (char *)malloxit(sizeof(char)
+		// 		* OPEN_MAX, alloc);
 		while (++j < OPEN_MAX)
 		{
 			(*map_info)->map[i][j] = '\0';
-			(*map_info)->map_tmp[i][j] = '\0';
+			// (*map_info)->map_tmp[i][j] = '\0';
 		}
 	}
 	(*map_info)->map[OPEN_MAX] = NULL;
+	(*map_info)->map_tmp[OPEN_MAX] = NULL;
 	(*map_info)->no = NULL;
 	(*map_info)->so = NULL;
 	(*map_info)->we = NULL;
 	(*map_info)->ea = NULL;
 	(*map_info)->f = -1;
 	(*map_info)->c = -1;
+	(*map_info)->count = 0;
+	(*map_info)->flag = 0;
+	(*map_info)->height = 0;
 }
 
 void	map_scan(t_game *game, char *argv)
@@ -56,10 +58,16 @@ void	map_scan(t_game *game, char *argv)
 			error_exit_free("Invalid map", NULL, game->alloc);
 		game->map_info->map[y] = ft_memcpy(game->map_info->map[y],
 				line, ft_strlen(line));
-		game->map_info->map_tmp[y] = ft_memcpy(game->map_info->map_tmp[y],
-				line, ft_strlen(line));
+		// game->map_info->map_tmp[y] = ft_memcpy(game->map_info->map_tmp[y],
+		// 		line, ft_strlen(line));
 		free(line);
 		y++;
+	}
+	for (int i = 0; i < OPEN_MAX; i++)
+	{
+		printf("%s\n", game->map_info->map[i]);
+		game->map_info->map_tmp[i] = ft_strdup(game->map_info->map[i], &(game->alloc));
+		printf("%p\n", game->map_info->map_tmp[i]);
 	}
 	if (y > OPEN_MAX)
 		error_exit_free("Invalid map", NULL, game->alloc);
