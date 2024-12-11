e198a0db622d77edf443d36a9cdcb29fb975860a
kokosnakam <unknown@c3r1s3.42tokyo.jp>
2024-12-11 13:12:14 +0900

    refa: true false player

--- src/map_check.c
+++ src/map_check.c
@@ -6,7 +6,7 @@
 /*   By: kosnakam <kosnakam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 13:29:22 by kosnakam          #+#    #+#             */
-/*   Updated: 2024/12/11 13:03:15 by kosnakam         ###   ########.fr       */
+/*   Updated: 2024/12/11 13:11:00 by kosnakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,13 +35,13 @@ int	spell_check(char spell)
 		|| spell == 'N' || spell == 'S' || spell == 'W' || spell == 'E')
 	{
 		if (spell == 'N' || spell == 'S' || spell == 'W' || spell == 'E')
-			return (1);
-		return (2);
+			return (TRUE);
+		return (PLAYER);
 	}
-	return (0);
+	return (FALSE);
 }
 
-int	map_spell_check(t_game *game, char **map)
+bool	map_spell_check(t_game *game, char **map)
 {
 	int	y;
 	int	x;
@@ -57,17 +57,17 @@ int	map_spell_check(t_game *game, char **map)
 			if (spell_check(map[y][x]) == 1)
 			{
 				if (flag)
-					return (1);
+					return (false);
 				flag = 1;
 				set_player_info(game, map[y][x], vector_init(x, y));
 			}
 			else if (spell_check(map[y][x]) == 0)
-				return (1);
+				return (false);
 		}
 	}
 	if (!flag)
-		return (1);
-	return (0);
+		return (false);
+	return (true);
 }
 
 void	map_check(t_game *game, t_map *map_info)
@@ -77,7 +77,7 @@ void	map_check(t_game *game, t_map *map_info)
 	if (!map_info->no || !map_info->so || !map_info->we || !map_info->ea
 		|| map_info->f == -1 || map_info->c == -1)
 		error_exit_free("Map information is not found", NULL, game->alloc);
-	if (map_spell_check(game, map_info->map))
+	if (!map_spell_check(game, map_info->map))
 		error_exit_free("Invalid map", NULL, game->alloc);
 	wall_check(map_info, (int)game->player.pos.y / TEXTURE_SIZE,
 		(int)game->player.pos.x / TEXTURE_SIZE);
