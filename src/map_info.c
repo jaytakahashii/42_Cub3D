9c12173cc9b80c5b5399cbcc3cab313d69207a46
kokosnakam <unknown@c3r1s3.42tokyo.jp>
2024-12-11 13:05:32 +0900

    bug: OPEN_MAX - 2

--- src/map_info.c
+++ src/map_info.c
@@ -6,7 +6,7 @@
 /*   By: kosnakam <kosnakam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 16:10:30 by kosnakam          #+#    #+#             */
-/*   Updated: 2024/12/06 15:12:51 by kosnakam         ###   ########.fr       */
+/*   Updated: 2024/12/11 12:42:52 by kosnakam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,7 +100,7 @@ void	read_map_info(int fd, t_game *game)
 			error_exit_free("Map is empty", NULL, game->alloc);
 		if (end_map_info(line))
 		{
-			if (ft_strlen(line) > OPEN_MAX)
+			if (ft_strlen(line) > OPEN_MAX - 2)
 				error_exit_free("Invalid map", NULL, game->alloc);
 			game->map_info->map[1] = ft_memcpy(game->map_info->map[1] + 1, line,
 					ft_strlen(line));
